80a57ad7bf2c0daca4887b84c218604635358780
Tim Henderson <tim.tadh@gmail.com>
2012-06-13 11:52:15 -0400

    made a setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='pyflwor',
+      version='git master',
+      description=(
+        'A object query system for arbitrary python objects..'
+      ),
+      author='Tim Henderson',
+      author_email='tim.tadh@gmail.com',
+      url='https://www.github.com/timtadh/pyflwor',
+      license='BSD',
+      packages=['pyflwor'],
+)
+
